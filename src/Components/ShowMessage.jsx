e8072c2a99eb9778a8ac5e2a3953532c17d87b60
BasemYahia402 <basemyahia70@gmail.com>
2024-07-02 14:13:31 +0300

    Add ShowMessage component

---
+++ src/Components/ShowMessage.jsx
@@ -0,0 +1,9 @@
+const ShowMessage = ({ message }) => {
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-bold">{message}</p>
+    </div>
+  );
+};
+
+export default ShowMessage;

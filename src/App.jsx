b369e3115fde5443f116b9c4ae53b1aa9adfc5f9
BasemYahia402 <basemyahia70@gmail.com>
2024-07-02 15:13:51 +0300

    Call all route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Country from "./pages/Country";
+import NoPage from "./pages/NoPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path=":country" element={<Country />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

b3efa4f1d9f0b76590de6c0038b8e97e716bc029
BasemYahia402 <basemyahia70@gmail.com>
2024-07-02 15:35:49 +0300

    Call ScrollButton component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import ScrollButton from "../components/ScrollButton";
 
 const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-100 font-inter dark:bg-gray-900 dark:text-gray-100">
       <Header />
-      <div className="container px-5 mx-auto">
+      <div className="container mx-auto px-5">
         <Outlet />
       </div>
+      <ScrollButton />
     </div>
   );
 };

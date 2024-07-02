68ce35e39000f94e25f7d495a558c8af0aaeaaa1
BasemYahia402 <basemyahia70@gmail.com>
2024-07-02 15:35:17 +0300

    Add ScrollButton component

---
+++ src/Components/ScrollButton.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
+  return (
+    <div className="fixed bottom-0 right-0 mx-8 mb-8">
+      <button
+        className={`rounded-full bg-gray-200 p-3 focus:outline-none ${
+          visible ? "block" : "hidden"
+        }`}
+        onClick={scrollToTop}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default ScrollButton;
